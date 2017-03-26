7b8a2daa5e6660e708e12aafba31ea083bcbfe6b
Michael <mail@michaelsimon.io>
2017-03-26 15:04:08 +0200

    boyscout

--- src/app/validators/BobNameValidator.ts
+++ src/app/validators/BobNameValidator.ts
@@ -1,11 +1,11 @@
 import {AbstractControl} from "@angular/forms";
 
 export class BobNameValidator {
-  static validate(c: AbstractControl): {[p: string]: any} {
-    if (c.value === null || c.value === undefined) {
+  static validate(control: AbstractControl): { [p: string]: any } {
+    if (control.value === null || control.value === undefined) {
       return null;
     }
-    return c.value.indexOf('Bob') === -1 ? null : {
+    return control.value.indexOf('Bob') === -1 ? null : {
         validateFirstName: {
           valid: false
         }
